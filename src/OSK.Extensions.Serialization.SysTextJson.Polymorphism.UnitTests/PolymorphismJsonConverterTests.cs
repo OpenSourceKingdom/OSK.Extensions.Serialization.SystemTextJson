fb9a6ff942d5ad4b60517c7459363318f924e313
BlankDev117 <mmmasent@gmail.com>
2024-12-10 23:35:17 -0500

    Updated for case sensitivity (OSK-5) (#6)
    Motivation
    ----
    Current package fails to find property names if they aren't matching case. This should be based on the JsonSerializationOptions rather than asusmed
    
    Modifications
    ----
    * Updated package for Utf8JsonReaderExtensions which includes new overload for case sensitivity
    * Updated PolymorphismJsonConverter to use new extension
    * Added UnitTest to validate failing use case is passing
    
    Result
    ----
    Case sensitivity on deserialization is respected based on JsonSerializationOptions settings

--- src/OSK.Extensions.Serialization.SysTextJson.Polymorphism.UnitTests/PolymorphismJsonConverterTests.cs
+++ src/OSK.Extensions.Serialization.SysTextJson.Polymorphism.UnitTests/PolymorphismJsonConverterTests.cs
@@ -8,8 +8,10 @@ using OSK.Serialization.Polymorphism;
 using OSK.Serialization.Polymorphism.Discriminators;
 using OSK.Serialization.Polymorphism.Models;
 using OSK.Serialization.Polymorphism.Ports;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
+using System.Text.Unicode;
 using Xunit;
 
 namespace OSK.Extensions.Serialization.SysTextJson.Polymorphism.UnitTests
@@ -57,7 +59,7 @@ namespace OSK.Extensions.Serialization.SysTextJson.Polymorphism.UnitTests
         #region End to End
 
         [Fact]
-        public async void Validate_ServiceCollectionExtensions()
+        public async Task Validate_ServiceCollectionExtensions()
         {
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddSystemTextJsonSerialization();
@@ -92,6 +94,65 @@ namespace OSK.Extensions.Serialization.SysTextJson.Polymorphism.UnitTests
             var serializer = provider.GetRequiredService<IJsonSerializer>();
             var data = await serializer.SerializeAsync(items);
             _ = await serializer.DeserializeAsync(data, items.GetType());
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Deserialization_JsonSerializationOptionCaseSensitivityValidation_ReturnsExpectedResult(bool enforceCaseSensitivity)
+        {
+            // Arrange
+            var mockEnumStrategy = new Mock<IPolymorphismEnumDiscriminatorStrategy>();
+            mockEnumStrategy.Setup(m => m.GetConcreteType(It.IsAny<PolymorphismAttribute>(),
+                It.IsAny<Type>(), It.IsAny<object>()))
+                .Returns((PolymorphismAttribute attribute, Type typeToConvert, object currentValue) =>
+                {
+                    switch (currentValue)
+                    {
+                        case 0:
+                            return typeof(TestChildA);
+                        case 1:
+                            return typeof(TestChildB);
+                        default:
+                            throw new InvalidCastException("Current value not an expected abstract type");
+                    }
+                });
+
+            var context = new PolymorphismContext(
+                PolymorphismAttribute.GetPolymorphismAttribute(typeof(TestAbstract)),
+                typeof(TestAbstract),
+                mockEnumStrategy.Object
+                );
+
+            _mockContextProvider.Setup(m => m.HasPolymorphismStrategy(It.Is<Type>(t => t == typeof(TestAbstract))))
+                .Returns(true);
+            _mockContextProvider.Setup(m => m.GetPolymorphismContext(It.Is<Type>(t => t == typeof(TestAbstract))))
+                .Returns(context);
+
+            var itemJson = $"[{{ \"A\": 1, \"B\": [ 1, 2, 3 ], \"Today\": \"2024-12-10T23:29:28.067736-05:00\", \"aBsTrAcTtYpE\": 0}}]";
+
+            var serializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = !enforceCaseSensitivity,
+                WriteIndented = true,
+                TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+            };
+            serializerOptions.Converters.Add(_converter);
+            serializerOptions.MakeReadOnly();
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(itemJson));
+
+            // Act
+
+            memoryStream.Position = 0;
+            try
+            {
+                _ = JsonSerializer.Deserialize(memoryStream, typeof(IEnumerable<TestAbstract>), serializerOptions);
+                Assert.False(enforceCaseSensitivity);
+            }
+            catch (Exception ex)
+            {
+                Assert.True(enforceCaseSensitivity);
+            }
         }
 
         [Fact]
